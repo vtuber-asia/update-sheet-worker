26b9e63cc7c872899cb368ee873b00f1cbd92adb
Naufal Fachrian <naufal.fachrian@gmail.com>
2023-08-03 15:33:32 +0700

    Upload sorted data

--- upload.py
+++ upload.py
@@ -16,6 +16,7 @@ class Upload:
         self.csv_filename = csv_filename
         self.logger.info(f"Uploading {self.csv_filename} to Google Sheet ...")
         data = self.data_from(self.csv_filename)
+        self.clear_data_on_sheet()
         return gspread_service().spreadsheets().values().batchUpdate(
             spreadsheetId=os.getenv("GOOGLE_SHEET_ID"),
             body={
@@ -23,6 +24,17 @@ class Upload:
                 'data': data,
             },
         ).execute()
+
+    def clear_data_on_sheet(self):
+        return gspread_service().spreadsheets().values().batchClear(
+            spreadsheetId=os.getenv("GOOGLE_SHEET_ID"),
+            body={
+                'ranges': self.cell_ranges(),
+            },
+        ).execute()
+
+    def cell_ranges(self) -> list:
+        pass
 
     def data_from(self, csv_filename) -> list:
         pass
--- upload_youtube.py
+++ upload_youtube.py
@@ -6,6 +6,11 @@ from content_platform import ContentPlatform
 
 class UploadYouTube(Upload):
 
+    def cell_ranges(self) -> list:
+        return [
+            'YouTube Ranking by Subscribers!A3:K',
+        ]
+
     def data_from(self, csv_filename) -> list:
         usernames = YouTube(self.session, self.logger).fetch_username_cells()
         with open(csv_filename, 'r', newline='', encoding='utf-8') as csvfile:
@@ -17,19 +22,7 @@ class UploadYouTube(Upload):
                 rows = list(filter(lambda row: row['username'] == ContentPlatform.remove_handler_from(
                     username), from_csv_youtube_channels))
                 if len(rows) > 0:
-                    cells.append([
-                        UploadYouTube.cell_username_from(rows[0]),
-                        UploadYouTube.cell_channel_id_from(rows[0]),
-                        UploadYouTube.cell_channel_title_from(rows[0]),
-                        UploadYouTube.cell_badge_from(rows[0]),
-                        UploadYouTube.cell_is_membership_active_from(rows[0]),
-                        UploadYouTube.cell_profile_image_url_from(rows[0]),
-                        UploadYouTube.cell_banner_image_url_from(rows[0]),
-                        UploadYouTube.cell_videos_count_from(rows[0]),
-                        UploadYouTube.cell_views_count_from(rows[0]),
-                        UploadYouTube.cell_subscribers_count_from(rows[0]),
-                        UploadYouTube.cell_timestamp_from(rows[0]),
-                    ])
+                    cells.append(UploadYouTube.map_to_cell_from(rows[0]))
                 else:
                     cells.append(['', '', '', '', '', '', '', '', '', '', ''])
             else:
@@ -37,72 +30,96 @@ class UploadYouTube(Upload):
         return [
             {
                 'range': 'Summary!G3:Q',
-                'values': cells
+                'values': cells,
+            },
+            {
+                'range': self.cell_ranges()[0],
+                'values': list(map(UploadYouTube.map_to_cell_from, from_csv_youtube_channels))
             }
         ]
+
+    @staticmethod
+    def map_to_cell_from(row) -> list:
+        return [
+            UploadYouTube.cell_username_from(row),
+            UploadYouTube.cell_channel_id_from(row),
+            UploadYouTube.cell_channel_title_from(row),
+            UploadYouTube.cell_badge_from(row),
+            UploadYouTube.cell_is_membership_active_from(row),
+            UploadYouTube.cell_profile_image_url_from(row),
+            UploadYouTube.cell_banner_image_url_from(row),
+            UploadYouTube.cell_videos_count_from(row),
+            UploadYouTube.cell_views_count_from(row),
+            UploadYouTube.cell_subscribers_count_from(row),
+            UploadYouTube.cell_timestamp_from(row),
+        ]
+
 
     @staticmethod
     def cell_username_from(row):
         if 'username' in row:
             return f'=hyperlink("https://youtube.com/{row["username"]}"; "@{row["username"]}")'
-        return None
+        return ''
 
     @staticmethod
     def cell_channel_id_from(row):
         if 'channel_id' in row:
             return row['channel_id']
-        return None
+        return ''
 
     @staticmethod
     def cell_channel_title_from(row):
         if 'channel_title' in row:
             return row['channel_title']
-        return None
+        return ''
 
     @staticmethod
     def cell_badge_from(row):
         if 'badges' in row:
-            return row['badges']
-        return None
+            if row['badges'] == 'CHECK_CIRCLE_THICK':
+                return '✅'
+            if row['badges'] == 'OFFICIAL_ARTIST_BADGE':
+                return '🎵'
+        return ''
 
     @staticmethod
     def cell_is_membership_active_from(row):
-        if 'is_membership_active' in row and row['is_membership_active'] is True:
-            return f'=image("https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Yellow_Star_with_rounded_edges.svg/42px-Yellow_Star_with_rounded_edges.svg.png"; 4; 24; 24)'
-        return None
+        if 'is_membership_active' in row and row['is_membership_active'].lower() == 'true':
+            return '⭐️'
+        return ''
 
     @staticmethod
     def cell_profile_image_url_from(row):
         if 'profile_image_url' in row:
             return f'=image("{row["profile_image_url"]}"; 4; 80; 80)'
-        return None
+        return ''
 
     @staticmethod
     def cell_banner_image_url_from(row):
         if 'banner_image_url' in row:
             return f'=image("{row["banner_image_url"]}"; 4; 73; 130)'
-        return None
+        return ''
 
     @staticmethod
     def cell_videos_count_from(row):
         if 'videos_count' in row:
             return row['videos_count']
-        return None
+        return ''
 
     @staticmethod
     def cell_views_count_from(row):
         if 'views_count' in row:
             return row['views_count']
-        return None
+        return ''
 
     @staticmethod
     def cell_subscribers_count_from(row):
         if 'subscribers_count' in row:
             return row['subscribers_count']
-        return None
+        return ''
 
     @staticmethod
     def cell_timestamp_from(row):
         if 'timestamp' in row:
             return row['timestamp']
-        return None
+        return ''
