2d04b914573ca4eda0374e8c93000c60bfab2a27
Naufal Fachrian <naufal.fachrian@gmail.com>
2023-08-22 14:55:57 +0700

    Add instagram functionality

---
+++ instagram.py
@@ -0,0 +1,126 @@
+import os
+from csv import DictReader, DictWriter
+from datetime import datetime
+from json import loads
+from logging import Logger
+
+from requests import Session
+
+from content_platform import ContentPlatform
+from gservices import gspread_service
+
+
+class Instagram(ContentPlatform):
+
+    def __init__(self, session: Session, logger: Logger):
+        super().__init__(session, logger)
+
+    def fetch_username_cells(self) -> list:
+        response = gspread_service().spreadsheets().values().get(
+            spreadsheetId=os.getenv("GOOGLE_SHEET_ID"),
+            range="Summary!BL3:BL"
+        ).execute()
+        if 'values' in response:
+            return list(map(ContentPlatform.cells_on, response['values']))
+        return []
+
+    def fetch_user(self, username: str) -> dict | None:
+        username = ContentPlatform.remove_handler_from(username)
+        self.logger.info(f"Fetching Instagram user info for @{username}")
+        response = self.session.get(
+            os.getenv('IG_API_ENDPOINT'),
+            headers={
+                'X-Ig-App-Id': os.getenv('IG_APP_ID'),
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+                'Accept-Language': 'en-US,en;q=0.9,ru;q=0.8',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Accept': '*/*'
+            },
+            params={
+                'username': username,
+            }
+        )
+        json = loads(response.text)
+        if 'status' in json and json['status'].lower() == 'ok' and 'data' in json and 'user' in json['data']:
+            json_data = json['data']['user']
+            return {
+                'username': username,
+                'name': Instagram.name_from(json_data),
+                'is_verified': Instagram.has_blue_checkmark_from(json_data),
+                'profile_image_url': Instagram.profile_image_from(json_data),
+                'followers_count': Instagram.followers_count_from(json_data),
+                'following_count': Instagram.following_count_from(json_data),
+                'post_count': Instagram.post_count_from(json_data),
+                'timestamp': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            }
+        return None
+
+    def create_csv(self) -> str:
+        csv_filename = f'{datetime.now().strftime("%Y%m%d%H%M%S")}_instagram.csv'
+        fields = [
+            'username',
+            'name',
+            'is_verified',
+            'profile_image_url',
+            'followers_count',
+            'following_count',
+            'post_count',
+            'timestamp',
+        ]
+        with open(csv_filename, 'w', newline='', encoding='utf-8') as csvfile:
+            w = DictWriter(csvfile, fieldnames=fields)
+            w.writeheader()
+            for username in self.fetch_usernames():
+                user = self.fetch_user(username)
+                if user is not None:
+                    w.writerow(user)
+            csvfile.close()
+        with open(csv_filename, 'r', newline='', encoding='utf-8') as csvfile:
+            from_csv = list(DictReader(csvfile))
+            csvfile.close()
+        with open(csv_filename, 'w', newline='', encoding='utf-8') as csvfile:
+            w = DictWriter(csvfile, fieldnames=fields)
+            w.writeheader()
+            w.writerows(
+                sorted(list(
+                    filter(lambda row: row is not None, from_csv)
+                ), key=lambda row: int(row['followers_count']), reverse=True)
+            )
+            csvfile.close()
+        return csv_filename
+
+    @staticmethod
+    def name_from(json_data) -> str:
+        if 'full_name' in json_data:
+            return json_data['full_name']
+        return ''
+
+    @staticmethod
+    def has_blue_checkmark_from(json_data) -> bool:
+        if 'is_verified' in json_data:
+            return json_data['is_verified']
+        return False
+
+    @staticmethod
+    def profile_image_from(json_data) -> str:
+        if 'profile_pic_url' in json_data:
+            return json_data['profile_pic_url']
+        return ''
+
+    @staticmethod
+    def followers_count_from(json_data) -> int:
+        if 'edge_followed_by' in json_data and 'count' in json_data['edge_followed_by']:
+            return json_data['edge_followed_by']['count']
+        return 0
+
+    @staticmethod
+    def following_count_from(json_data) -> int:
+        if 'edge_follow' in json_data and 'count' in json_data['edge_follow']:
+            return json_data['edge_follow']['count']
+        return 0
+
+    @staticmethod
+    def post_count_from(json_data) -> int:
+        if 'edge_owner_to_timeline_media' in json_data and 'count' in json_data['edge_owner_to_timeline_media']:
+            return json_data['edge_owner_to_timeline_media']['count']
+        return 0
---
+++ upload_instagram.py
@@ -0,0 +1,116 @@
+import os
+from csv import DictReader
+
+from content_platform import ContentPlatform
+from instagram import Instagram
+from upload import Upload
+
+
+class UploadInstagram(Upload):
+
+    def cell_ranges(self) -> list:
+        return [
+            'Instagram Ranking by Followers!A3:M',
+        ]
+    
+    def data_from(self, csv_filename) -> list:
+        username = Instagram(self.session, self.logger).fetch_username_cells()
+        with open(csv_filename, 'r', newline='', encoding='utf-8') as csvfile:
+            from_csv = list(DictReader(csvfile))
+            csvfile.close()
+        cells = []
+        for username in username:
+            if username is not None:
+                rows = list(filter(lambda row: row['username'].lower() == ContentPlatform.remove_handler_from(
+                    username).lower(), from_csv))
+                if len(rows) > 0:
+                    cells.append(UploadInstagram.map_to_cell_from(rows[0]))
+                else:
+                    cells.append(['', '', '', '', '', '', '', ''])
+            else:
+                cells.append(['', '', '', '', '', '', '', ''])
+        return [
+            {
+                'range': 'Summary!BM3:BT',
+                'values': cells,
+            },
+            {
+                'range': self.cell_ranges()[0],
+                'values': list(map(UploadInstagram.map_to_cell_with_xlookup_from, from_csv))
+            }
+        ]
+    
+    @staticmethod
+    def map_to_cell_from(row) -> list:
+        return [
+            UploadInstagram.cell_username_from(row),
+            UploadInstagram.cell_name_from(row),
+            UploadInstagram.cell_is_verified_from(row),
+            UploadInstagram.cell_profile_image_url_from(row),
+            UploadInstagram.cell_followers_count_from(row),
+            UploadInstagram.cell_following_count_from(row),
+            UploadInstagram.cell_post_count_from(row),
+            UploadInstagram.cell_timestamp_from(row),
+        ]
+    
+    @staticmethod
+    def map_to_cell_with_xlookup_from(row) -> list:
+        cells = UploadInstagram.map_to_cell_from(row)
+        cells.extend(
+            [
+                f'=XLOOKUP("@{row["username"]}";Summary!$BM$3:$BM;Summary!$B$3:$B)',
+                f'=XLOOKUP("@{row["username"]}";Summary!$BM$3:$BM;Summary!$C$3:$C)',
+                f'=XLOOKUP("@{row["username"]}";Summary!$BM$3:$BM;Summary!$D$3:$D)',
+                f'=XLOOKUP("@{row["username"]}";Summary!$BM$3:$BM;Summary!$E$3:$E)',
+                f'=XLOOKUP("@{row["username"]}";Summary!$BM$3:$BM;Summary!$F$3:$F)',
+            ]
+        )
+        return cells
+    
+    @staticmethod
+    def cell_username_from(row) -> str:
+        if 'username' in row:
+            return f'=hyperlink("https://instagram.com/{row["username"]}"; "@{row["username"]}")'
+        return ''
+    
+    @staticmethod
+    def cell_name_from(row) -> str:
+        if 'name' in row:
+            return row['name']
+        return ''
+    
+    @staticmethod
+    def cell_is_verified_from(row):
+        if 'is_verified' in row and row['is_verified'] and row['is_verified'].lower() == 'true':
+            return f'=image("{os.getenv("TWITTER_BLUE_BADGE_URL")}"; 4; 20; 20)'
+        return ''
+    
+    @staticmethod
+    def cell_profile_image_url_from(row):
+        if 'profile_image_url' in row and row['profile_image_url']:
+            return f'=image("{row["profile_image_url"]}")'
+        return ''
+    
+    @staticmethod
+    def cell_followers_count_from(row):
+        if 'followers_count' in row:
+            return row['followers_count']
+        return ''
+    
+    @staticmethod
+    def cell_following_count_from(row):
+        if 'following_count' in row:
+            return row['following_count']
+        return ''
+    
+    @staticmethod
+    def cell_post_count_from(row):
+        if 'post_count' in row:
+            return row['post_count']
+        return ''
+    
+    @staticmethod
+    def cell_timestamp_from(row):
+        if 'timestamp' in row:
+            return row['timestamp']
+        return ''
