9a594f4e67a8ad27796121f40c62278423292258
Naufal Fachrian <naufal.fachrian@gmail.com>
2023-08-29 11:07:59 +0700

    Implement log to file

--- setup.py
+++ setup.py
@@ -5,11 +5,18 @@ from dotenv import load_dotenv
 from requests import Session
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
+from datetime import datetime
 
 
 def setup(package_name: str, log_level: int) -> (Session, Logger):
     load_dotenv()
-    logging.basicConfig(level=log_level)
+    logging.basicConfig(
+        filename=f'{datetime.now().strftime("%Y%m%d%H%M%S")}.log',
+        filemode='a',
+        format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+        datefmt='%H:%M:%S',
+        level=log_level
+    )
     logger = logging.getLogger(package_name)
     session = Session()
     retry = Retry(connect=10, backoff_factor=0.5)
