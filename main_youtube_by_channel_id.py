6b7b7ae3ca8c4c112647a627a9fa83c726eb9a1e
Naufal Fachrian <naufal.fachrian@gmail.com>
2023-09-02 21:21:04 +0700

    Add YouTube by channel ID

---
+++ main_youtube_by_channel_id.py
@@ -0,0 +1,30 @@
+import logging
+from datetime import datetime
+
+from setup import setup
+from upload_link import UploadLink
+from upload_youtube import UploadYouTube
+from youtube_by_channel_ids import YouTubeByChannelIds
+
+if __name__ == "__main__":
+    start = datetime.now()
+
+    session, logger = setup("id.bungamungil.vtuber-asia-v3.main", logging.INFO)
+
+    youTube = YouTubeByChannelIds(session, logger)
+    youTube_csv = youTube.create_csv()
+    upload_youtube = UploadYouTube(session, logger)
+    logger.info(upload_youtube.upload(youTube_csv))
+    upload_link = UploadLink(session, logger)
+    logger.info(upload_link.upload(youTube_csv))
+    del youTube
+    del youTube_csv
+    del upload_youtube
+    del upload_link
+
+    del session
+
+    end = datetime.now()
+
+    logger.info(f"Duration: {end - start}")
+    del logger
