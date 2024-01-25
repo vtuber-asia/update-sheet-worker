ac290642a366d3892a3a16c27f1058828606aa1f
Naufal Fachrian <naufal.fachrian@gmail.com>
2024-01-25 14:14:39 +0700

    Update database config

--- database.py
+++ database.py
@@ -1,38 +1,30 @@
-import json
-import os.path
-from peewee import *
-from errors import DatabaseConfigNotFound, UndefinedDatabaseConnectionType
-from dotenv import load_dotenv
+from os import getenv
+from peewee import SqliteDatabase, MySQLDatabase, PostgresqlDatabase
+from errors import UndefinedDatabaseConnectionType
 
 
 def connect_db():
-    load_dotenv()
-    DB_CONFIG_FILE = os.getenv("DB_CONFIG_FILE")
-    if os.path.isfile(DB_CONFIG_FILE):
-        db_config_file = open(DB_CONFIG_FILE)
-        db_config = json.load(db_config_file)
-        db_config_file.close()
-        connection_type = db_config['connection']
-        database_name = db_config['database']
-        if connection_type == "sqlite":
-            return SqliteDatabase(database_name)
-        if connection_type == "mysql":
-            return MySQLDatabase(database_name,
-                                 user=db_config['username'],
-                                 password=db_config['password'],
-                                 host=db_config['host'],
-                                 port=db_config['port']
-                                 )
-        if connection_type == "postgres":
-            return PostgresqlDatabase(database_name,
-                                      user=db_config['username'],
-                                      password=db_config['password'],
-                                      host=db_config['host'],
-                                      port=db_config['port']
-                                      )
-        raise UndefinedDatabaseConnectionType
-    else:
-        raise DatabaseConfigNotFound
+    connection_type = getenv('DB_TYPE')
+    database_name = getenv('DB_NAME')
+    if connection_type == "sqlite":
+        return SqliteDatabase(database_name)
+    if connection_type == "mysql":
+        return MySQLDatabase(
+            database_name,
+            user=getenv('DB_USER'),
+            password=getenv('DB_PASS'),
+            host=getenv('DB_HOST'),
+            port=int(getenv('DB_PORT'))
+        )
+    if connection_type == "postgres":
+        return PostgresqlDatabase(
+            database_name,
+            user=getenv('DB_USER'),
+            password=getenv('DB_PASS'),
+            host=getenv('DB_HOST'),
+            port=getenv('DB_PORT')
+        )
+    raise UndefinedDatabaseConnectionType
 
 
 db = connect_db()
