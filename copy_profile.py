d8d7f024b74dd1740225eaf25c81396c37226e0e
Naufal Fachrian <naufal.fachrian@gmail.com>
2024-02-14 20:05:50 +0700

    Add copy profile

---
+++ copy_profile.py
@@ -0,0 +1,84 @@
+from os import getenv
+from copy_cells import fetch_cells_from, write_cells_to
+
+
+if __name__ == '__main__':
+    dest_spreadsheet_id = getenv('DEST_SPREADSHEET_ID')
+    source_spreadsheet_id = getenv('SOURCE_SPREADSHEET_ID')
+    print(write_cells_to(
+            dest_spreadsheet_id,
+            "Profile!A:E",
+            "USER_ENTERED",
+            fetch_cells_from(
+                source_spreadsheet_id,
+                "DataSource!A:E",
+                "FORMULA",
+            )
+        )
+    )
+    print(write_cells_to(
+            dest_spreadsheet_id,
+            "Profile!F:F",
+            "RAW",
+            fetch_cells_from(
+                source_spreadsheet_id,
+                "DataSource!G:G",
+                "FORMULA",
+            )
+        )
+    )
+    print(write_cells_to(
+            dest_spreadsheet_id,
+            "Profile!G:G",
+            "RAW",
+            fetch_cells_from(
+                source_spreadsheet_id,
+                "DataSource!T:T",
+                "FORMULA",
+            )
+        )
+    )
+    print(write_cells_to(
+            dest_spreadsheet_id,
+            "Profile!H:H",
+            "RAW",
+            fetch_cells_from(
+                source_spreadsheet_id,
+                "DataSource!AB:AB",
+                "FORMULA",
+            )
+        )
+    )
+    print(write_cells_to(
+            dest_spreadsheet_id,
+            "Profile!I:I",
+            "RAW",
+            fetch_cells_from(
+                source_spreadsheet_id,
+                "DataSource!AN:AN",
+                "FORMULA",
+            )
+        )
+    )
+    print(write_cells_to(
+            dest_spreadsheet_id,
+            "Profile!J:J",
+            "RAW",
+            fetch_cells_from(
+                source_spreadsheet_id,
+                "DataSource!AX:AX",
+                "FORMULA",
+            )
+        )
+    )
+    print(write_cells_to(
+            dest_spreadsheet_id,
+            "Profile!K:K",
+            "RAW",
+            fetch_cells_from(
+                source_spreadsheet_id,
+                "DataSource!BL:BL",
+                "FORMULA",
+            )
+        )
+    )
